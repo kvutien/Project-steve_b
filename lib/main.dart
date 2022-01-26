491d1ac29cc5834252237693888b69f0421529fa
kvutien <kvutien.yes@gmail.com>
2022-01-26 13:19:30 +0100

    cleanup comments in main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -5,12 +5,12 @@ import 'package:flutter_inappwebview/flutter_inappwebview.dart';
 import 'package:url_launcher/url_launcher.dart';
 
 Future main() async {
+  // binding to Binary Messenger, for Flutter to call platform-specific API
   WidgetsFlutterBinding.ensureInitialized();
-
   if (Platform.isAndroid) {
     await AndroidInAppWebViewController.setWebContentsDebuggingEnabled(true);
   }
-
+  // the usual runApp starts here
   runApp(const MyApp());
 }
 
@@ -21,10 +21,12 @@ class MyApp extends StatefulWidget {
 }
 
 class _MyAppState extends State<MyApp> {
+  // this key makes any widget in the widget tree access the WebView state
   final GlobalKey webViewKey = GlobalKey();
 
   InAppWebViewController? webViewController;
   InAppWebViewGroupOptions options = InAppWebViewGroupOptions(
+    // see all options in https://inappwebview.dev/docs/in-app-webview/webview-options/
       crossPlatform: InAppWebViewOptions(
         useShouldOverrideUrlLoading: true,
         mediaPlaybackRequiresUserGesture: false,
@@ -36,7 +38,7 @@ class _MyAppState extends State<MyApp> {
         allowsInlineMediaPlayback: true,
       ));
 
-  late PullToRefreshController pullToRefreshController;
+  late PullToRefreshController pullToRefreshController; // refresh display
   String url = "";
   final String urlSteveBGee = 'https://ibisa.users.earthengine.app/view/mekongsalinity';
   double progress = 0;
@@ -51,6 +53,7 @@ class _MyAppState extends State<MyApp> {
         color: Colors.blue,
       ),
       onRefresh: () async {
+        // refresh WebView display
         if (Platform.isAndroid) {
           webViewController?.reload();
         } else if (Platform.isIOS) {
@@ -78,20 +81,7 @@ class _MyAppState extends State<MyApp> {
           ),
           body: SafeArea(
               child: Column(children: <Widget>[
-                /*TextField(  // the URL filed is not shown in Steve_B app
-              decoration: const InputDecoration(prefixIcon: Icon(Icons.search)),
-              controller: urlController,
-              keyboardType: TextInputType.url,
-              onSubmitted: (value) {
-                var url = Uri.parse(value); // parse the input field as an URL
-                if (url.scheme.isEmpty) {
-                  // if it doesn't look like URL, it's a search keyword
-                  url = Uri.parse("https://www.google.com/search?q=" + value);
-                }
-                // if it is URL, load the web page
-                webViewController?.loadUrl(urlRequest: URLRequest(url: url));
-              },
-            ),*/
+                // the URL field is not shown in Steve_B app
                 Expanded( // area to display web content
                   child: Stack( // stack widgets one above another
                     children: [
@@ -135,7 +125,7 @@ class _MyAppState extends State<MyApp> {
                             "about"
                           ].contains(uri.scheme)) {
                             if (await canLaunch(url)) {
-                              // Launch the App
+                              // Launch the Web App
                               await launch(
                                 url,
                               );
@@ -177,6 +167,7 @@ class _MyAppState extends State<MyApp> {
                         },
                       ),
                       // if the web page is still loading, show progress bar
+                      //  on top of the webview display
                       progress < 1.0
                           ? LinearProgressIndicator(value: progress)
                           : Container(),
@@ -184,154 +175,7 @@ class _MyAppState extends State<MyApp> {
                   ),
                 ),
                 // the demo button bar is not used in Steve_B app
-                /*ButtonBar(
-              alignment: MainAxisAlignment.center,
-              children: <Widget>[
-                ElevatedButton(
-                  child: Icon(Icons.arrow_back),
-                  onPressed: () {
-                    webViewController?.goBack();
-                  },
-                ),
-                ElevatedButton(
-                  child: Icon(Icons.arrow_forward),
-                  onPressed: () {
-                    webViewController?.goForward();
-                  },
-                ),
-                ElevatedButton(
-                  child: Icon(Icons.refresh),
-                  onPressed: () {
-                    webViewController?.reload();
-                  },
-                ),
-              ],
-            ),*/
               ]))),
     );
   }
 }
-// -----------------------
-/*
-// import 'package:flutter/cupertino.dart';
-import 'package:flutter/material.dart';
-import 'package:webview_flutter/webview_flutter.dart';
-import 'MyWebView.dart';
-
-void main() {
-  runApp(const MyApp());
-}
-
-class MyApp extends StatelessWidget {
-  const MyApp({Key? key}) : super(key: key);
-
-  // This widget is the root of your application.
-  @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Steve_B Earth Observer',
-      theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // Try running your application with "flutter run". You'll see the
-        // application has a blue toolbar. Then, without quitting the app, try
-        // changing the primarySwatch below to Colors.green and then invoke
-        // "hot reload" (press "r" in the console where you ran "flutter run",
-        // or simply save your changes to "hot reload" in a Flutter IDE).
-        // Notice that the counter didn't reset back to zero; the application
-        // is not restarted.
-        primarySwatch: Colors.blue,
-      ),
-      home: const MyHomePage(title: 'Steve_B Earth Observer'),
-    );
-  }
-}
-
-class MyHomePage extends StatelessWidget {
-  const MyHomePage({Key? key, required this.title}) : super(key: key);
-
-  final String title;
-
-  // MyWebView _myWebView = new MyWebView(
-  //   title: "Mont Saint-Michel",
-  //   selectedUrl: "https://ibisatraining.users.earthengine.app/view/montstmichel",
-  // );
-
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-        appBar: null,
-        body: const WebView(
-          initialUrl: 'https://ibisa.users.earthengine.app/view/mekongsalinity',
-          javascriptMode: JavascriptMode.unrestricted,
-        ));
-  }
-}
-
-*/
-/*class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
-
-  void _incrementCounter() {
-    setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      _counter++;
-    });
-  }
-
-  @override
-  Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
-    return Scaffold(
-      appBar: AppBar(
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
-        title: Text(widget.title),
-      ),
-      body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
-        child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Invoke "debug painting" (press "p" in the console, choose the
-          // "Toggle Debug Paint" action from the Flutter Inspector in Android
-          // Studio, or the "Toggle Debug Paint" command in Visual Studio Code)
-          // to see the wireframe for each widget.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            const Text(
-              'You have pushed the button this many times:',
-            ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headline4,
-            ),
-          ],
-        ),
-      ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
-    );
-  }
-}*/
